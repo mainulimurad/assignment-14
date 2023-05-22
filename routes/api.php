5af7ee485d8543322be67b58bf25e111aa5369ae
Mainul I Murad <mainulimurad@gmail.com>
2023-05-22 23:35:57 +0600

    final commit

--- routes/api.php
+++ routes/api.php
@@ -12,22 +12,21 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request){
 // Question 1
 Route::post('/userinfo',[AssignmentController::class,'UserInfo']);
 
-// Question 2
+
 Route::post('/useragent',[AssignmentController::class,'UserAgent']);
 
-// Question 3
 Route::get('/query',[AssignmentController::class,'ApiEndpoint']);
 
-// Question 4
+
 Route::get('/jsonresponse',[AssignmentController::class,'JsonResponse']);
 
-// Question 5
+
 Route::post('/upload',[AssignmentController::class,'FileUploads']);
 
-// Question 6
+
 Route::post('/setcookie',[AssignmentController::class,'SetCookie']);
 
-// Question 7
+
 Route::post('/submit',function(Request $request){
     $email = $request->input('email');
     if($email){
